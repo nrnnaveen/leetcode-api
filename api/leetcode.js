e0592e36f6641dc8ee71e87eb7cebd2f979adeeb
M.NAVEEN <naveen.ai25@stellamaryscoe.edu.in>
2026-07-10 10:57:11 +0530

    Create leetcode.js

---
+++ api/leetcode.js
@@ -0,0 +1,53 @@
+export default async function handler(req, res) {
+  const username = req.query.username;
+
+  if (!username) {
+    return res.status(400).json({
+      error: "Username required",
+    });
+  }
+
+  const response = await fetch("https://leetcode.com/graphql", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "User-Agent": "Mozilla/5.0",
+    },
+    body: JSON.stringify({
+      query: `
+        query getUserProfile($username: String!) {
+          matchedUser(username: $username) {
+            submitStats {
+              acSubmissionNum {
+                difficulty
+                count
+              }
+            }
+          }
+        }
+      `,
+      variables: {
+        username,
+      },
+    }),
+  });
+
+  const json = await response.json();
+
+  const stats =
+    json?.data?.matchedUser?.submitStats?.acSubmissionNum;
+
+  if (!stats) {
+    return res.status(404).json({
+      error: "User not found",
+    });
+  }
+
+  const all = stats.find(
+    (x) => x.difficulty === "All"
+  );
+
+  return res.status(200).json({
+    totalSolved: all.count,
+  });
+}
